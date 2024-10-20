900a104398242cfaddc96fd842165f603e63d3a0
kha-cell <adekanbikhalid62@gmail.com>
2024-10-20 23:28:25 +0100

    java assignment

---
+++ java assignment/NaturalNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+	public class NaturalNumber {
+	public static void main(String[]  args) {
+	Scanner input = new Scanner(System.in);
+	System.out.print("enter fisrt number:");
+	int number1 = input.nextInt();
+	System.out.print("enter second number:");
+	int number2 = input.nextInt();
+	System.out.print("enter third number:");
+	int number3 = input.nextInt();
+	System.out.print("enter fourth number:");
+	int number4 = input.nextInt();
+	System.out.print("enter fifth number:");
+	int number5 = input.nextInt();
+	System.out.print("enter sixth number:");
+	int number6 = input.nextInt();
+	System.out.print("enter seventh number:");
+	int number7 = input.nextInt();
+	System.out.print("enter eighth number:");
+	int number8 = input.nextInt();
+	System.out.print("enter nineth number:");
+	int number9 = input.nextInt();
+	System.out.print("enter tenth number:");
+	int number10 = input.nextInt();
+	int sum = number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10;
+	System.out.print(sum);
+  }
+}
+	
+	

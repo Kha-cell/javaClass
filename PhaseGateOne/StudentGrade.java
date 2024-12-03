9aee34d4a9ae0ab51d7f5664158603823509d72c
kha-cell <adekanbikhalid62@gmail.com>
2024-12-03 15:21:10 +0100

    PhaseGateOne

--- PhaseGateOne/StudentGrade.java
+++ PhaseGateOne/StudentGrade.java
@@ -5,10 +5,9 @@ public class StudentGrade {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-       	 System.out.print("how many student do you have: ");
+       	 System.out.print("welcome to lagbaja school\nhow many student do you have: ");
        	 int numberOfStudents = input.nextInt();
-		System.out.println("saving>>>>>>>>>>>>>>>>>");
-		System.out.println("saved succesfully ");
+		
        	 System.out.print("how many subject do each student offer: ");
        	 int numberOfSubjects = input.nextInt(); 
 		System.out.println("saving>>>>>>>>>>>>>>>>>");
@@ -19,22 +18,30 @@ public class StudentGrade {
 
         	int[][] scores = new int[numberOfStudents][numberOfSubjects];
 
- for (int i = 0; i < numberofstudent; i++) {
+ for (int i = 0; i < numberOfStudents; i++) {
             System.out.println("Enter scores for student " + (i + 1) + ":");
-            for (int j = 0; j < numberofsubject; j++) {
+            for (int f = 0; f < numberOfSubjects; f++) {
                 while (true) {
-                    System.out.print("Subject " + (j + 1) + ": ");
+                    System.out.print("Subject " + (f + 1) + ": ");
                     int score = input.nextInt();
                     if (score >= 0 && score <= 100) {
-                        scores[i][j] = score;
+                        scores[i][f] = score;
                         break;
                     } else {
-                        System.out.println("Score must be between 0 and 100. Please enter again.");
+                        System.out.println("Score out of range input score from 1-100." );
                     }
       
           }
 
       }
-    }
-  }
+   }
+   		System.out.println("================================================================================================================================================");
+
+
+
+
+
+
+
+       }
 }
